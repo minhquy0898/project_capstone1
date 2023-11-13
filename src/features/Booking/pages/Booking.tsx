00b87bf8671b5c39a285e09cb1bbef536c9981af
NhungDTH <nhungdth@liftsoft.vn>
2023-11-13 21:15:17 +0700

    add form register info

--- src/features/Booking/pages/Booking.tsx
+++ src/features/Booking/pages/Booking.tsx
@@ -2,30 +2,70 @@ import { FormProvider } from 'react-hook-form';
 import * as Yup from 'yup';
 import useFormWithYup from '../../../hooks/useFormWithYup';
 import TableBooking from '../components/TableBooking';
+import CInput from '../../../components/CInput';
+import CDatePicker from '../../../components/CDatePicker/CDatepicker';
+import { Button } from '@nextui-org/react';
+import { format } from 'date-fns';
 
 const bookingSchema = Yup.object().shape({
-  event: Yup.string().required('Vui lòng chọn loại dịch vụ!'),
-  address: Yup.string().required('Vui lòng nhập địa chỉ!'),
-  phone: Yup.string().required('Vui lòng nhập số điện thoại!'),
+  // customerName: Yup.string().required('Vui lòng chọn loại dịch vụ!'),
+  // email: Yup.string().required('Vui lòng nhập địa chỉ!'),
+  // numberOfAttendes: Yup.string().required('Vui lòng nhập số điện thoại!'),
 });
 
 function Booking() {
   const methods = useFormWithYup(bookingSchema, {
     defaultValues: {
-      event: null,
-      address: '',
-      phone: '',
+      customerName: '',
+      email: '',
+      phoneNumber: '',
+      numberOfAttendes: '',
+      eventTime: null,
     },
   });
   const { handleSubmit } = methods;
 
-  const submitHandler = handleSubmit((values) => {});
+  const submitHandler = handleSubmit((values) => {
+    console.log('values', values);
+    const newScheduledTime = values.eventTime
+      ? format(values.eventTime as unknown as Date, 'yyyy/MM/dd hh:mm')
+      : '';
+  });
 
   return (
     <div>
       <FormProvider {...methods}>
         <form onSubmit={submitHandler}>
+          <label>THÔNG TIN KHÁCH HÀNG</label>
+          <div className="w-1/3">
+            <CInput
+              label="Tên khách hàng"
+              name="customerName"
+              id="customerName"
+            />
+
+            <CInput
+              label="Địa chỉ email"
+              name="email"
+              id="email"
+              type="email"
+            />
+            <CInput label="Số điện thoại" name="phoneNumber" id="phoneNumber" />
+
+            <CInput
+              label="Lượng khách dự kiến"
+              name="numberOfAttendes"
+              id="phoneNumber"
+            />
+            <CDatePicker
+              name="eventTime"
+              placeholderText="Nhập thời gian tổ chức"
+            />
+          </div>
           <TableBooking />
+          <Button className="mt-5" type="submit">
+            Submit
+          </Button>
         </form>
       </FormProvider>
     </div>

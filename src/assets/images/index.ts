3d990bf3f317e4a854640b5189fd224ddb05a955
Haupt <th06072002@gmail.com>
2023-11-30 19:42:22 +0700

    fix introduce

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,9 @@
 import bgEvent from './bg-su-kien.jpeg';
+import bgIntroduce from './z4763430132617_5f02d03c1c016b0dce88c63076d4e103 1.png';
 
 const APP_IMAGES = {
   bgEvent,
+  bgIntroduce,
 };
 
 export default APP_IMAGES;
